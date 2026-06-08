d530e1039508e55bcc5a4221692e94ccaf2a4e26
NatanMakielkowski <32420@s.pm.szczecin.pl>
2026-06-08 12:34:12 +0200

    feat: initial structure

---
+++ backend/src/main/java/com/example/arena_booker/ArenaBookerApplication.java
@@ -0,0 +1,13 @@
+package com.example.arena_booker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArenaBookerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArenaBookerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/arena_booker/ArenaBookerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.arena_booker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArenaBookerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
