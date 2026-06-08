2160d7a6e783b7dc06c1f6154a0ba73692038b4e
Natan-Makielkowski <natanmakielkowski@gmail.com>
2026-06-08 12:58:54 +0200

    feat: added AppUser model

---
+++ backend/src/main/java/com/example/arena_booker/model/AppUser.java
@@ -0,0 +1,24 @@
+package com.example.arena_booker.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class AppUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String username;
+    private String password;
+    private String role;
+    public AppUser() {}
+    public AppUser(String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+}
