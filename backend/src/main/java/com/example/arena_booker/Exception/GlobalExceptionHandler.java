e735a953408bdd9889ce8d9f1f17522cb7808b94
Natan-Makielkowski <natanmakielkowski@gmail.com>
2026-06-08 18:34:24 +0200

    feat: Exception handling

---
+++ backend/src/main/java/com/example/arena_booker/Exception/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.example.arena_booker.Exception;
+
+
+import com.example.arena_booker.dto.ErrorResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponseDto> handleResourceNotFoundException(ResourceNotFoundException ex){
+        ErrorResponseDto errorDto = new ErrorResponseDto(LocalDateTime.now(), HttpStatus.NOT_FOUND.value(), HttpStatus.NOT_FOUND.getReasonPhrase(), ex.getMessage());
+        return new ResponseEntity<>(errorDto, HttpStatus.NOT_FOUND);
+    }
+
+
+}
---
+++ backend/src/main/java/com/example/arena_booker/Exception/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.example.arena_booker.Exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(String message){
+        super(message);
+    }
+
+}
