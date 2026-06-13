829175fe26dd08cdcb4f53704225cf347b6ccfac
natan-makielkowski <natanmakielkowski@gmail.com>
2026-06-13 14:31:31 +0200

    modified:   frontend/lib/models/reservation.dart

--- frontend/lib/models/reservation.dart
+++ frontend/lib/models/reservation.dart
@@ -1,10 +1,10 @@
 import 'sector.dart';
-class Reservation{
+
+class Reservation {
   final String id;
   final Sector sector;
   final DateTime startTime;
   final DateTime endTime;
-
 
   Reservation({
     required this.id,
@@ -13,4 +13,15 @@ class Reservation{
     required this.endTime,
   });
 
+  static Reservation fromJson(Map<String, dynamic> json) {
+    return Reservation(
+      id: json['id'].toString(),
+      sector: Sector.values.firstWhere(
+        (e) => e.name == json['sector'],
+        orElse: () => Sector.sectorA,
+      ),
+      startTime: DateTime.parse(json['startTime']),
+      endTime: DateTime.parse(json['endTime']),
+    );
+  }
 }
