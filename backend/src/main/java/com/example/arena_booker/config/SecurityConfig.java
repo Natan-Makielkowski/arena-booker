93bfbb063e4f3fba8925f52416d16f37590e2fe1
Natan-Makielkowski <natanmakielkowski@gmail.com>
2026-06-18 18:48:22 +0200

    feat: added swagger API documentation

--- backend/src/main/java/com/example/arena_booker/config/SecurityConfig.java
+++ backend/src/main/java/com/example/arena_booker/config/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.POST, "/api/login").permitAll()
                         .requestMatchers("/api/users/**").hasRole("ADMIN")
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
